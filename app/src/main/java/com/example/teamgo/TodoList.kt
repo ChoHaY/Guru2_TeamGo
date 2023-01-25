d46c4a00ab2d190bb0478d76f69bd75647a99d1a
ChoHaY <youngj_86@naver.com>
2023-01-25 17:52:20 +0900

    20230125(17:50)_detail_delete_check

--- app/src/main/java/com/example/teamgo/TodoList.kt
+++ app/src/main/java/com/example/teamgo/TodoList.kt
@@ -1,16 +1,23 @@
 package com.example.teamgo
 
+import android.annotation.SuppressLint
+import android.content.DialogInterface
 import android.content.Intent
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import android.view.ContextThemeWrapper
+import android.view.View
 import android.widget.ImageButton
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import net.flow9.thisisKotlin.firebase.R
 
-class TodoList : AppCompatActivity() {
+class TodoList() : AppCompatActivity(){
 
     lateinit var dbManager: DBManager
     lateinit var sqlitedb : SQLiteDatabase
@@ -18,7 +25,9 @@ class TodoList : AppCompatActivity() {
     lateinit var pjname: TextView
     lateinit var pjdate: TextView
     lateinit var back: ImageButton
+    lateinit var delete: ImageButton
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_todo_list)
@@ -35,13 +44,17 @@ class TodoList : AppCompatActivity() {
         dbManager = DBManager(this,"projectlistDB", null, 1)
         sqlitedb = dbManager.readableDatabase
 
-        //val intent = intent
+        val intent = intent
+        var str_name: String
+        var str_date: String
+
+        str_name = intent.getStringExtra("intent_PJ_name").toString()
+
         var cursor: Cursor
-        cursor = sqlitedb.rawQuery("SELECT * FROM projectlist;",null)
+        cursor = sqlitedb.rawQuery("SELECT * FROM projectlist WHERE PJName ='"+str_name+"';",null)
 
         if(cursor.moveToNext()){
-            var str_name: String = intent.getStringExtra("intent_PJ_name").toString()
-            var str_date: String = intent.getStringExtra("intent_PJ_date").toString()
+            str_date = intent.getStringExtra("intent_PJ_date").toString()
 
             pjname = findViewById(R.id.project_name3)
             pjname.text = str_name
@@ -53,5 +66,31 @@ class TodoList : AppCompatActivity() {
         cursor.close()
         sqlitedb.close()
         dbManager.close()
+
+        //////////////////////////////////////////////
+
+        delete = findViewById(R.id.DeletePj_btn)
+        delete.setOnClickListener{
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("프로젝트 삭제")
+                .setMessage("프로젝트를 삭제하시겠습니까?")
+                .setPositiveButton("예",DialogInterface.OnClickListener{dialog, which->
+                    dbManager = DBManager(this,"projectlistDB", null, 1)
+                    sqlitedb = dbManager.readableDatabase
+                    sqlitedb.execSQL("DELETE FROM projectlist WHERE PJName ='"+str_name+"';")
+                    sqlitedb.close()
+                    dbManager.close()
+
+                    val intent = Intent(this,TodayProject::class.java)
+                    startActivity(intent)
+                })
+                .setNegativeButton("아니오",DialogInterface.OnClickListener{dialog, which->
+                    builder.setOnDismissListener(object : DialogInterface.OnDismissListener {
+                        override fun onDismiss(arg0: DialogInterface) {}
+                    })
+                })
+                .setCancelable(false)
+            builder.show()
+        }
     }
 }
