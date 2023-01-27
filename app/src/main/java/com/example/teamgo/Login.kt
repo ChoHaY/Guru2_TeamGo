0d5f356018a61a628a170cba93420a2568ea60fa
ChoHaY <youngj_86@naver.com>
2023-01-28 03:42:34 +0900

    2023.01.28(03:30) layout_login_edit

--- app/src/main/java/com/example/teamgo/Login.kt
+++ app/src/main/java/com/example/teamgo/Login.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -16,6 +17,8 @@ class Login : AppCompatActivity() {
     lateinit var putPassword: EditText
     lateinit var Loginbtn: Button
     lateinit var auth: FirebaseAuth
+    lateinit var putID: EditText
+    lateinit var check: CheckBox
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,51 +27,69 @@ class Login : AppCompatActivity() {
 
         auth = FirebaseAuth.getInstance()
 
-        putEmail = findViewById(R.id.InputEmail)
+        putEmail = findViewById(R.id.InputID)
         putPassword = findViewById(R.id.InputPassword)
         Loginbtn = findViewById(R.id.Login_btn)
+        putID = findViewById(R.id.InputID)
+        check = findViewById(R.id.AuthLogin_check)
 
         Loginbtn.setOnClickListener {
+            var ID = putID.text.toString()
+            var CheckBox = check.isChecked()
             var Email = putEmail.text.toString()
             var Password = putPassword.text.toString()
 
-            auth.createUserWithEmailAndPassword(Email, Password)
+            auth.createUserWithEmailAndPassword(Email,Password)
                 .addOnCompleteListener { result ->
                     if (result.isSuccessful) {
                         if (auth.currentUser != null) {
+                            saveDate(ID,CheckBox)
                             Toast.makeText(this, "회원가입 완료", Toast.LENGTH_SHORT).show()
                             var intent = Intent(this, TodayProject::class.java)
                             startActivity(intent)}
                         } else if (result.exception?.message.isNullOrEmpty()) {
                             Toast.makeText(this, "오류가 발생", Toast.LENGTH_SHORT).show()
                         } else {
+                            saveDate(ID,CheckBox)
                             Toast.makeText(this, "로그인 완료", Toast.LENGTH_SHORT).show()
                             login(Email.toString(), Password.toString())
                             var intent = Intent(this, TodayProject::class.java)
                             startActivity(intent)
                         }
+
             }
         }
-            auth = FirebaseAuth.getInstance()
-            var currentUser = auth?.currentUser //이미 로그인한적이 있는지 확인합니다.
 
-            if (currentUser == null) {
-                /*Timer().schedule(object : TimerTask() {
-                    override fun run() {
-                        val intent: Intent = Intent(applicationContext, Login::class.java)
-                        startActivity(intent)
-                        finish()
-                    }
-                }, 1500)*/
-            } else {
-                Timer().schedule(object : TimerTask() {
-                    override fun run() {
-                        val intent: Intent = Intent(applicationContext, TodayProject::class.java)
-                        startActivity(intent)
-                        finish()
-                    }
-                }, 1500)
-            }
+        val pref = getSharedPreferences("LoginInfo", 0)
+        val savedC =pref.getBoolean("BOX",false)
+
+        if(savedC == true){
+            val intent: Intent = Intent(applicationContext, TodayProject::class.java)
+            startActivity(intent)
+            finish()
+            Toast.makeText(this, "자동 로그인", Toast.LENGTH_SHORT).show()
+        }
+
+        /*auth = FirebaseAuth.getInstance()
+        var currentUser = auth?.currentUser //이미 로그인한적이 있는지 확인합니다.
+
+        if (currentUser == null) {
+            /*Timer().schedule(object : TimerTask() {
+                override fun run() {
+                    val intent: Intent = Intent(applicationContext, Login::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }, 1500)*/
+        } else {
+            Timer().schedule(object : TimerTask() {
+                override fun run() {
+                    val intent: Intent = Intent(applicationContext, TodayProject::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }, 1500)
+        }*/
     }
     fun login(Email: String, Password: String) {
         auth.signInWithEmailAndPassword(Email, Password) // 로그인
@@ -79,5 +100,14 @@ class Login : AppCompatActivity() {
                   startActivity(intent)
               }
         }
+    }
+    fun saveDate( loginID :String, checkBox:Boolean ){
+        val pref =getSharedPreferences("LoginInfo", MODE_PRIVATE) //shared key 설정
+        val edit = pref.edit() // 수정모드
+
+        edit.putString("ID", loginID) // 값 넣기
+        edit.putBoolean("BOX", checkBox)
+
+        edit.apply() // 적용하기
     }
 }
