4d29ae6d69cec2efa7d9f6a12679408fa49f1518
parkkass <gkjh621@swu.ac.kr>
2023-01-29 19:00:58 +0900

    2023.01.29.19:00

--- app/src/main/java/com/example/teamgo/Splash.kt
+++ app/src/main/java/com/example/teamgo/Splash.kt
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.WindowManager
 import net.flow9.thisisKotlin.firebase.R
 
 class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.a_splash)
 
