a0e661159b0f504f8880f47c53a12592ecedff14
parkkass <gkjh621@swu.ac.kr>
2023-01-29 20:23:33 +0900

    2023.01.29.20:00

--- app/src/main/java/com/example/teamgo/Splash.kt
+++ app/src/main/java/com/example/teamgo/Splash.kt
@@ -12,6 +12,7 @@ class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.a_splash)
 
         // 일정 시간 후 실행 함수
@@ -20,8 +21,8 @@ class Splash : AppCompatActivity() {
             // 2초 후 로그인 화면으로 이동
             val intent = Intent(this, Login::class.java)
             startActivity(intent)
+            overridePendingTransition(com.google.android.material.R.anim.abc_fade_in, com.google.android.material.R.anim.abc_fade_out)
             finish()
-
         } ,2000)
     }
 }
