5afeb42ddfb0f2d4f81ca81a562d7d6ab9e816fe
Yoojin11 <utwoma@naver.com>
2023-01-29 20:56:01 +0900

    bottom

--- app/src/main/java/com/example/teamgo/Login.kt
+++ app/src/main/java/com/example/teamgo/Login.kt
@@ -3,8 +3,6 @@ package com.example.teamgo
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
@@ -22,82 +20,70 @@ class Login : AppCompatActivity() {
     lateinit var putID: EditText
     lateinit var check: CheckBox
 
-        @SuppressLint("MissingInflatedId")
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.login)
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
 
-            auth = FirebaseAuth.getInstance()
+        auth = FirebaseAuth.getInstance()
 
-            putEmail = findViewById(R.id.InputID)
-            putPassword = findViewById(R.id.InputPassword)
-            Loginbtn = findViewById(R.id.Login_btn)
-            putID = findViewById(R.id.InputID)
-            check = findViewById(R.id.AuthLogin_check)
+        putEmail = findViewById(R.id.InputID)
+        putPassword = findViewById(R.id.InputPassword)
+        Loginbtn = findViewById(R.id.Login_btn)
+        putID = findViewById(R.id.InputID)
+        check = findViewById(R.id.AuthLogin_check)
 
-            val pref = getSharedPreferences("LoginInfo", 0)
-            val savedID = pref.getString("ID", "").toString()
-            val savedC = pref.getBoolean("BOX", false)
+        val pref = getSharedPreferences("LoginInfo", 0)
+        val savedID = pref.getString("ID","").toString()
+        val savedC =pref.getBoolean("BOX",false)
 
+        Loginbtn.setOnClickListener {
+            var ID = putID.text.toString()
+            var CheckBox = check.isChecked()
+            var Email = putEmail.text.toString()
+            var Password = putPassword.text.toString()
 
-
-            Loginbtn.setOnClickListener {
-                var ID = putID.text.toString()
-                var CheckBox = check.isChecked()
-                var Email = putEmail.text.toString()
-                var Password = putPassword.text.toString()
-
-                auth.createUserWithEmailAndPassword(Email, Password)
-                    .addOnCompleteListener { result ->
-                        if (result.isSuccessful) {
-                            if (auth.currentUser != null) {
-                                saveDate(ID, CheckBox)
-                                Toast.makeText(this, "회원가입 완료", Toast.LENGTH_SHORT).show()
-                                var intent = Intent(this, TodayProject::class.java)
-                                intent.putExtra("UserID", savedID)
-                                startActivity(intent)
-                            }
+            auth.createUserWithEmailAndPassword(Email,Password)
+                .addOnCompleteListener { result ->
+                    if (result.isSuccessful) {
+                        if (auth.currentUser != null) {
+                            saveDate(ID,CheckBox)
+                            Toast.makeText(this, "회원가입 완료", Toast.LENGTH_SHORT).show()
+                            var intent = Intent(this, TodayProject::class.java)
+                            intent.putExtra("UserID",savedID)
+                            startActivity(intent)}
                         } else if (result.exception?.message.isNullOrEmpty()) {
                             Toast.makeText(this, "오류가 발생", Toast.LENGTH_SHORT).show()
                         } else {
-                            saveDate(ID, CheckBox)
+                            saveDate(ID,CheckBox)
                             Toast.makeText(this, "로그인 완료", Toast.LENGTH_SHORT).show()
                             login(Email.toString(), Password.toString())
                             var intent = Intent(this, TodayProject::class.java)
-                            intent.putExtra("UserID", savedID)
+                            intent.putExtra("UserID",savedID)
                             startActivity(intent)
                         }
 
-                    }
-                }
-
-
-                if (savedC == true) {
-                    val intent: Intent = Intent(applicationContext, TodayProject::class.java)
-                    intent.putExtra("UserID", savedID)
-                    startActivity(intent)
-                    finish()
-                    Handler(Looper.getMainLooper()).postDelayed({
-                        Toast.makeText(this, "자동 로그인", Toast.LENGTH_SHORT).show()
-                    },500)
-                }
-
-
+            }
         }
 
-                fun login(Email: String, Password: String) {
-                    auth.signInWithEmailAndPassword(Email, Password) // 로그인
-                        .addOnCompleteListener { result ->
-                            if (result.isSuccessful) {
-                                Toast.makeText(this, "로그인 완료", Toast.LENGTH_SHORT).show()
-                                var intent = Intent(this, TodayProject::class.java)
-                                startActivity(intent)
-                            }
-                        }
-                }
-
-
-
+       if(savedC == true){
+            val intent: Intent = Intent(applicationContext, TodayProject::class.java)
+            intent.putExtra("UserID",savedID)
+            startActivity(intent)
+            finish()
+            Toast.makeText(this, "자동 로그인", Toast.LENGTH_SHORT).show()
+        }
+    }
+    fun login(Email: String, Password: String) {
+        auth.signInWithEmailAndPassword(Email, Password) // 로그인
+             .addOnCompleteListener { result ->
+              if (result.isSuccessful) {
+                  Toast.makeText(this, "로그인 완료", Toast.LENGTH_SHORT).show()
+                  var intent = Intent(this, TodayProject::class.java)
+                  startActivity(intent)
+              }
+        }
+    }
     fun saveDate( loginID :String, checkBox:Boolean ){
         val pref =getSharedPreferences("LoginInfo", MODE_PRIVATE) //shared key 설정
         val edit = pref.edit() // 수정모드
